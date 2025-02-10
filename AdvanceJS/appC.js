b94455ada1017619252dd0b19075ef3b36d1b108
Muhammad Arslan <imarslan7061@gmail.com>
2025-02-10 23:30:08 +0500

    update
    Learn About ES6 Modules & Common Js

---
+++ AdvanceJS/appC.js
@@ -0,0 +1,5 @@
+const mathOperation = require("./MathOperationCommon.js")
+
+console.log(mathOperation.add(10,40))
+console.log(mathOperation.subtract(900, 890))
+console.log(mathOperation.multiply(20, 5))
---
+++ AdvanceJS/appM.js
@@ -0,0 +1,11 @@
+// Default Import
+import division from "./MathOperationModul.js";
+
+// Named Import
+import { add, subtract, multiply } from "./MathOperationModul.js";
+
+
+console.log(add(10,20))
+console.log(subtract(30, 10))
+console.log(multiply(5, 10))
+console.log(division(100, 5))
