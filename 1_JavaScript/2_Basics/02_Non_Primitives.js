16b8b1cf9cd9b9c6ef3bd57bc00ba9e4724ec7c8
Muhammad Arslan <imarslan7061@gmail.com>
2025-10-17 23:38:00 +0500

    Add

--- 1_JavaScript/2_Basics/02_Non_Primitives.js
+++ 1_JavaScript/2_Basics/02_Non_Primitives.js
@@ -1,13 +1,19 @@
 const userInfo = {
-    userName: "Arslan",
-    age: 20,
-    job:"Software Developer",
-    hobbies: ['Reading', 'GYM', 'Cycling', 'Coding'],
-    "is working": true
-}
-console.log("Obj Before Modify", userInfo)
+  userName: "Arslan",
+  age: 20,
+  job: "Software Developer",
+  hobbies: ["Reading", "GYM", "Cycling", "Coding"],
+  "is working": true,
+};
+console.log("Obj Before Modify", userInfo);
 
-userInfo.userName = "HM-Arslan"
-console.log(userInfo["is working"])
+userInfo.userName = "HM-Arslan";
+console.log(userInfo["is working"]);
 
-console.log("Obj After Modify", userInfo)
+console.log("Obj After Modify", userInfo);
+
+let fruits = ["Apple", "Banana", "Mango"];
+let isValue = "10abc";
+console.log(Number(isValue));
+console.log(Number(undefined));
+console.log(typeof NaN);
