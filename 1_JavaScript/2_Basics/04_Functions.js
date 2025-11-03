9b9cb89134a8f5e6a919a2ed3ff521fef1a5924e
Arslan <imArslan7061@gmail.com>
2025-11-04 00:46:40 +0500

    JS FOundations

--- 1_JavaScript/2_Basics/04_Functions.js
+++ 1_JavaScript/2_Basics/04_Functions.js
@@ -5,10 +5,39 @@ function makeTea (typeOfTea) {
 let teaOrder = makeTea("Green Tea")
 
 //  Exercise 2:
-function orderTea (teaType) {
+function orderTea(teaType) {
     function confirmOrder() {
         return `Order Confirmed for ${teaType}`
     }
-    return confirmOrder()   
+    return confirmOrder()
 }
-console.log(orderTea("Lemon Tea"))
+let orderComfirmation = orderTea("Chai")
+// console.log(orderComfirmation)
+
+function giveMe() {
+    let arr =  [1,2,3,4,5,6,7,8]
+    // console.log(arr)
+    return arr
+}
+
+// giveMe()
+// .map((item) => console.log(item))
+// console.log(orderTea("Lemon Tea"))
+
+//  Exercise 3:
+let calculateTotal = (price, quantity) => {
+    return Number(price * quantity)
+}
+let totalCost = calculateTotal(400, 5)
+// console.log(totalCost)
+
+//  Exercise 4:
+function makeTea(teaType) {
+    return teaType
+}
+function processTeaOrder(makeTea) {
+    return makeTea("Earl Grey")
+}
+
+let res = processTeaOrder(makeTea)
+console.log(res)
